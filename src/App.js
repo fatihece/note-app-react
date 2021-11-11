545f281e8b3a50619627432f7972d89b5b4aa2b2
fatih <fatihenaz@gmail.com>
2021-11-11 05:36:39 +0300

    Basic structure of Note done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import NotesList from './components/NotesList';
 
 function App() {
   return (
     <div className="App">
-    <h1>My Notes App</h1>
+      <h1>My Notes App</h1>
+      <NotesList />
     </div>
   );
 }

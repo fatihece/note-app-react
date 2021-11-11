6ab1994c224e54acc3847ea60c316bc538abfbf3
fatih <fatihenaz@gmail.com>
2021-11-11 05:59:19 +0300

    modified css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import { useState } from "react";
 import NotesList from './components/NotesList';
 
 function App() {
+  const[notes, setNotes] = useState("")
   return (
-    <div className="App">
+    <div className="container">
       <h1>My Notes App</h1>
       <NotesList />
     </div>
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -5,6 +5,9 @@ const NotesList = () => {
     return (
         <div className="notes-list">
            <Note />
+           <Note />
+           <Note />
+           <Note />
         </div>
     )
 }
